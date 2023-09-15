22dbce816b3714fd8470fd4d58588d122b02c6f7
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2023-09-15 13:51:12 -0500

    feat: shadcn components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import "@/styles/globals.css";
 
 import { type AppType } from "next/app";
 
+import { Toaster } from "@/components/ui/toaster";
 import { api } from "@/lib/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
