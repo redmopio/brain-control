73d06b61451df56cd487d1fa619217a5abdc14dc
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2023-09-16 00:54:22 -0500

    feat: prettier sort imports

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options & import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "@ianvs/prettier-plugin-sort-imports",
+  ],
+  pluginSearchDirs: false,
+
+  importOrder: ["<THIRD_PARTY_MODULES>", "", "^@/(.*)$", "", "^[./]"],
 };
 
 export default config;
